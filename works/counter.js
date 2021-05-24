7681f26f54e0c105d2bbb563624b74416ded59fb
saiten <saitenntaisei@gmail.com>
2021-05-24 10:11:57 +0900

    コメント削除
    コンポーネント化した後に残った古いコードを削除

--- works/counter.js
+++ works/counter.js
@@ -2,15 +2,11 @@ let count = 0
 const countUp = () => {
   count++
   viewcounter();
-  //const countElement = document.querySelector('#count')
-  //countElement.innerText = '回数: ' + count
 }
 
 const reset=()=>{
   count=0;
   viewcounter();
-  //const countElement = document.querySelector('#count')
-  //countElement.innerText = '回数: ' + count
 }
 
 function viewcount () {
