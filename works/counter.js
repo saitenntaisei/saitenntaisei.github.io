ca3f84de734c908ed2e108f7c71fab2ef23f7861
saiten <saitenntaisei@gmail.com>
2021-05-24 10:05:47 +0900

    Update counter.js

--- works/counter.js
+++ works/counter.js
@@ -1,12 +1,19 @@
 let count = 0
 const countUp = () => {
   count++
-  const countElement = document.querySelector('#count')
-  countElement.innerText = '回数: ' + count
+  viewcounter();
+  //const countElement = document.querySelector('#count')
+  //countElement.innerText = '回数: ' + count
 }
 
 const reset=()=>{
   count=0;
+  viewcounter();
+  //const countElement = document.querySelector('#count')
+  //countElement.innerText = '回数: ' + count
+}
+
+function viewcount () {
   const countElement = document.querySelector('#count')
   countElement.innerText = '回数: ' + count
 }
