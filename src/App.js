274fec561e9370f2a147b7fad6213122fbbdc37c
saitenntaisei <saitenntaisei@gmail.com>
2023-11-07 00:57:52 +0900

    :construction: Roughly complete

--- src/App.js
+++ src/App.js
@@ -21,7 +21,11 @@ function Section({ sectionTitle }) {
 function Itroduction() {
   return (
     <div className="flex justify-center items-center">
-      <img className="w-64 h-64  relative" src="saiten.png" alt="icon" />
+      <img
+        className="object-contain w-64 h-64  relative"
+        src="saiten.png"
+        alt="icon"
+      />
       <div className="text-black text-2xl font-semibold font-mono text-left shadow-md border border-gray-200	 rounded">
         <ul className="list-disc list-inside m-5">
           <li>saiten</li>
@@ -45,6 +49,47 @@ function Itroduction() {
   );
 }
 
+function WorkedList() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="text-black text-2xl font-semibold font-mono text-left shadow-inner border	 rounded">
+        <ul className="mx-20 my-7 list-disc list-inside">
+          <li> pixiv 2022.4-2022.10</li>
+          <li>DeNA 2023.9 (3 days)</li>
+          <li>Wantedly 2023.9 (3 weeks)</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+function Links() {
+  return (
+    <div className="flex justify-center items-center space-x-10">
+      <a href="https://twitter.com/sort_reverse">
+        <img
+          className="object-contain w-10 h-10  relative"
+          src="x.png"
+          alt="Twitter"
+        />
+      </a>
+      <a href="https://github.com/saitenntaisei">
+        <img
+          className="object-contain w-10 h-10  relative"
+          src="github.png"
+          alt="github"
+        />
+      </a>
+      <a href="https://qiita.com/saitenntaisei">
+        <img
+          className="object-contain w-10 h-10  relative"
+          src="qiita.png"
+          alt="qiita"
+        />
+      </a>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
@@ -54,7 +99,9 @@ function App() {
       <Section sectionTitle="About" />
       <Itroduction />
       <Section sectionTitle="Works" />
+      <WorkedList />
       <Section sectionTitle="Links" />
+      <Links />
     </div>
   );
 }
