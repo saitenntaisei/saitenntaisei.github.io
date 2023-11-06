d2f62ab6af5b77575f4ea980549b531a225f6cac
saitenntaisei <saitenntaisei@gmail.com>
2023-11-07 00:19:47 +0900

    :construction: Finish about

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Header() {
+  return (
+    <div className="shadow-md  flex justify-center">
+      <h1 className="text-black	 text-5xl	 font-mono	 m-7">
+        <span>saiten's Portfolio</span>
+      </h1>
+    </div>
+  );
+}
+function Section({ sectionTitle }) {
+  return (
+    <div>
+      <h1 className="text-black text-center text-4xl font-medium font-mono underline m-7 ">
+        <span>{sectionTitle}</span>
+      </h1>
+    </div>
+  );
+}
+function Itroduction() {
+  return (
+    <div className="flex justify-center items-center">
+      <img className="w-64 h-64  relative" src="saiten.png" alt="icon" />
+      <div className="text-black text-2xl font-semibold font-mono text-left shadow-md border border-gray-200	 rounded">
+        <ul className="list-disc list-inside m-5">
+          <li>saiten</li>
+          <li>
+            Status
+            <ul className="list-disc list-inside ml-7">
+              <li>Tokyo Institute of Technology</li>
+              <li>B3</li>
+            </ul>
+          </li>
+          <li>
+            Affiliation
+            <ul className="list-disc list-inside ml-7">
+              <li>デジタル創作同好会traP</li>
+              <li>ロボット技術研究会Rogy </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
+        <Header />
       </header>
+      <Section sectionTitle="About" />
+      <Itroduction />
+      <Section sectionTitle="Works" />
+      <Section sectionTitle="Links" />
     </div>
   );
 }
