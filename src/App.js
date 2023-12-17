cc2bb625211396493de881424c4520f0d65a8370
saitenntaisei <saitenntaisei@gmail.com>
2023-12-18 01:28:19 +0900

    :bug: Fix bug of smartphone view

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ function Links() {
 
 function App() {
   return (
-    <div className="App bg-white dark:bg-slate-800 h-screen">
+    <div className="App bg-white dark:bg-slate-800 h-fit">
       <header className="">
         <Header />
       </header>
