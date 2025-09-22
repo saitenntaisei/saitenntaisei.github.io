1d949f53d4d70d87f55d40f07c9b0e39f2e704ff
saitenntaisei <saitenntaisei@gmail.com>
2025-09-22 17:58:19 +0900

    feat: remvoe some text

--- src/PersonalProject.jsx
+++ src/PersonalProject.jsx
@@ -8,12 +8,6 @@ function PersonalProject() {
       <h1 className="text-black dark:text-slate-200 text-4xl font-mono font-semibold mb-6">
         Personal Projects
       </h1>
-      <div className="max-w-3xl w-full px-6">
-        <p className="text-black dark:text-slate-300 text-lg leading-7">
-          Here are some of my personal projects. This section can feature
-          descriptions, screenshots, and links to repositories or live demos.
-        </p>
-      </div>
       <div className="w-full flex flex-col items-center mt-4">
         <VFDGPSClockPage />
         <NixiedClockPage />
